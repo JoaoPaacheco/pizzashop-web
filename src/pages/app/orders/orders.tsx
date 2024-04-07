646d860cce17ae8a10b0f33808578da2200f23fd
Joao Pedro Pacheco Rodrigues Almeida <joao_pra@hotmail.com>
2024-04-07 13:26:45 -0300

    Página: Listagen de pedidos (completa)

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,3 +1,4 @@
+import { ArrowRight, Search, X } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 
 import { Button } from '@/components/ui/button'
@@ -22,38 +23,68 @@ export function Orders() {
         <form action="" className="flex items-center gap-2">
           <span className="text-sm font-semibold">Filtros:</span>
           <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-
-          <div className="rounded-md border">
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead></TableHead>
-                  <TableHead>Identificador</TableHead>
-                  <TableHead>Realizado há</TableHead>
-                  <TableHead>Status</TableHead>
-                  <TableHead>Cliente</TableHead>
-                  <TableHead>Total de pedidos</TableHead>
-                  <TableHead></TableHead>
-                  <TableHead></TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                <TableRow>
-                  <TableCell>
-                    <Button></Button>
-                  </TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </div>
         </form>
+
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[64px]"></TableHead>
+                <TableHead className="w-[140px]">Identificador</TableHead>
+                <TableHead className="w-[180px]">Realizado há</TableHead>
+                <TableHead className="w-[140px]">Status</TableHead>
+                <TableHead>Cliente</TableHead>
+                <TableHead className="w-[140px]">Total do pedido</TableHead>
+                <TableHead className="w-[164px]"></TableHead>
+                <TableHead className="w-[132px]"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {Array.from({ length: 10 }).map((_, i) => {
+                return (
+                  <TableRow key={i}>
+                    <TableCell>
+                      <Button variant="outline" size="xs">
+                        <Search className="h-3 w-3" />
+                        <span className="sr-only">Detalhes do pedido</span>
+                      </Button>
+                    </TableCell>
+                    <TableCell className="font-mono text-xs font-medium">
+                      821e78f7asdhdf128h
+                    </TableCell>
+                    <TableCell className="text-muted-foreground">
+                      há 15 minutos
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex items-center gap-2">
+                        <span className="h-2 w-2 rounded-full bg-slate-400" />
+                        <span className="font-medium text-muted-foreground">
+                          Pendente
+                        </span>
+                      </div>
+                    </TableCell>
+                    <TableCell className="font-medium">
+                      João Pedro Pacheco
+                    </TableCell>
+                    <TableCell className="font-medium">R$ 149,90</TableCell>
+                    <TableCell>
+                      <Button variant="outline" size="xs">
+                        <ArrowRight className="mr-2 h-3 w-3" />
+                        Aprovar
+                      </Button>
+                    </TableCell>
+                    <TableCell>
+                      <Button variant="ghost" size="xs">
+                        <X className="mr-2 h-3 w-3" />
+                        Cancelar
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </div>
       </div>
     </>
   )
